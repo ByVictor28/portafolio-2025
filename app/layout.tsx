1a1f720bcb93aeada8371609a1811feb8f1ed990
DESKTOP-HNKVQFB\Pc <victor delfin>
2025-10-22 00:51:11 -0600

    Add Google Tag Manager integration to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Exo_2 } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+import Script from "next/script";
 
 const exo2 = Exo_2({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -22,9 +23,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* <body className={`antialiased`}> */}
+      {/* <!-- Google Tag Manager --> */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K9Z7SQDN')`,
+        }}
+      ></Script>
+
       <body
         className={`!${exo2.variable} !font-exo2 antialiased text-text-primary`}
       >
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K9Z7SQDN"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
